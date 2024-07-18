3af9a53f7c1494526c0122499f76e2b866e26ca0
AlifferDaniel <alifferdaniel14@gmail.com>
2024-07-17 23:44:24 -0300

    17/07

---
+++ Python/10.Exercicio_If.py
@@ -0,0 +1,8 @@
+primeiro_numero = input('Digite um número: ')
+segundo_numero = input('Digite outro número: ')
+
+if primeiro_numero >= segundo_numero : 
+    print(f'{primeiro_numero=} é maior ou igual a {segundo_numero=}')
+
+else :
+ print(f'{segundo_numero=} é maior ou igual a {primeiro_numero=}')
